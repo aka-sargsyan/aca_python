c9e32b3b3c012288a41f50acb9fff9b248266a5c
aka-sargsyan <akasargsyan87@gmail.com>
2022-06-21 01:56:13 +0400

    added previous_song,show_current_song and str function in Player

--- src/player/Player/Player.py
+++ src/player/Player/Player.py
@@ -11,7 +11,8 @@ class Player:
         self.__current_song_index = 0
 
     def play(self, index=None):
-        if not index:
+        if index is None:
+            print(self.__current_song_index)
             index = self.__current_song_index
         elif index >= len(self.pl.song):
             index = len(self.pl.song) - 1
@@ -37,3 +38,31 @@ class Player:
             self.play(self.__current_song_index + 1)
         else:
             print('player can not play song, you must play it')
+
+        print(self.__current_song_index)
+
+    def previous_song(self):
+        if self.__is_playing:
+            if self.__current_song_index > 0:
+                self.play(self.__current_song_index - 1)
+            else:
+                self.play(self.__current_song_index)
+        else:
+            print('player can not play song, you must play it')
+
+    def show_current_song(self):
+        print(self.pl.song[self.__current_song_index])
+
+    def __str__(self):
+        if not self.pl.song:
+            return 'list of song empty'
+        else:
+            text = ''
+
+            for i in range(len(self.pl.song)):
+                if i == self.__current_song_index:
+                    text += self.pl.song[i].name + '........play.........' + '\n'
+                else:
+                    text += self.pl.song[i].name + '\n'
+            return text
+
